1a632bf32a095f3e6deff7bb55ba482e888de671
Prince <prince15046@iiitd.ac.in>
2020-03-29 17:08:52 +0530

    First code

---
+++ Assets/FirstButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class FirstButton : MonoBehaviour
+{	
+
+    public GameObject earLeft_mesh;
+    public GameObject earRight_mesh; 
+    // Start is called before the first frame update
+    void Start()
+    {
+      earLeft_mesh = GameObject.Find("earLeft_mesh");
+      earRight_mesh = GameObject.Find("earRight_mesh");
+      //Debug.Log("Hello: " + earLeft_mesh.name);
+      //Debug.Log("Hello: " + earRight_mesh.name);
+    }
+
+    // Update is called once per frame
+    //void Update()
+    //{        
+    //}
+     	
+
+     public void changeMaterial(Material inputmaterial){	
+	earLeft_mesh.GetComponent<SkinnedMeshRenderer>().material = inputmaterial;
+	earRight_mesh.GetComponent<SkinnedMeshRenderer>().material = inputmaterial;
+     }	
+
+}
---
+++ Assets/SecondButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecondButton : MonoBehaviour
+{
+    public GameObject earLeft_mesh;
+    public GameObject earRight_mesh; 
+    public GameObject FaceTexture; 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+      earLeft_mesh = GameObject.Find("earLeft_mesh");
+      earRight_mesh = GameObject.Find("earRight_mesh");
+      FaceTexture = GameObject.Find("FaceTexture");
+      
+      //Debug.Log("Hello: " + earLeft_mesh.name);
+      //Debug.Log("Hello: " + earRight_mesh.name);
+    }
+
+    // Update is called once per frame
+    //void Update()
+    //{        
+    //}
+     	
+
+     public void changeMaterial(Material inputmaterial){
+
+	FaceTexture.GetComponent<MeshRenderer>().material = inputmaterial;
+	earLeft_mesh.SetActive(false);
+	earRight_mesh.SetActive(false);
+
+     }	
+}
